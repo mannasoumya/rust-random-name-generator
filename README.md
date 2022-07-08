2389eeec2ed360a64710fae9697a4b78f6d6f39d
Manna Soumya <sm323265@gmail.com>
2022-07-08 15:13:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Random Name Generator in Rust
+
+## Quick Start
+
+```console
+$ cargo run
+    Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/random_name_generator`
+
+Defaulting to 6 characters length and 6 total words
+UFOWUK
+EBUBOD
+UYAWIP
+UVISAN
+EMOPUH
+AXUTAD
+$ cargo run help
+    Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/random_name_generator help`
+
+Usage:
+cargo run <how-many-words> <number-of-characters>
+
+<how-many-words>       : integer
+<number-of-characters> : integer (Note: <number-of-characters> must be even)
+
+```
---
+++ src/main.rs
@@ -0,0 +1,79 @@
+extern crate rand;
+use rand::*;
+use std::env;
+use std::process;
+#[allow(non_snake_case)]
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (how_many, size_of_str) = validate_args(args);
+    generate_all(how_many, size_of_str);
+}
+
+fn validate_args(args: Vec<String>) -> (i32, i32) {
+    let len: i32 = args.len() as i32;
+    if len == 1 {
+        println!("\nDefaulting to 6 characters length and 6 total words");
+        return (6, 6);
+    }
+    for arg in &args {
+        if arg == "help" {
+            println!("\nUsage:\ncargo run <how-many-words> <number-of-characters>");
+            println!("\n<how-many-words>       : integer");
+            println!(
+                "<number-of-characters> : integer (Note: <number-of-characters> must be even)\n"
+            );
+            process::exit(0x0000);
+        }
+    }
+    let how_many: i32;
+    let size_of_str: i32;
+    if args[1].parse::<i32>().is_ok() {
+        how_many = args[1].parse::<i32>().unwrap();
+    } else {
+        println!(
+            "\nCannot parse i32: `{}`. <how-many-words> must be an integer",
+            args[1]
+        );
+        process::exit(0x0010);
+    }
+    if args[2].parse::<i32>().is_ok() {
+        size_of_str = args[2].parse::<i32>().unwrap();
+    } else {
+        println!(
+            "\nCannot parse i32: `{}` . <number-of-characters> must be an integer",
+            args[2]
+        );
+        process::exit(0x0010);
+    }
+
+    return (how_many, size_of_str);
+}
+
+fn generate(n: i32) -> String {
+    let vowels = vec!['A', 'E', 'I', 'O', 'U'];
+    let consonants = vec![
+        'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W',
+        'X', 'Y', 'Z',
+    ];
+    let len_vow: i32 = vowels.len() as i32;
+    let len_con: i32 = consonants.len() as i32;
+    let mut res = String::new();
+    for _ in 0..n / 2 {
+        let mut rng = rand::thread_rng();
+        let index_vow = rng.gen_range(0, len_vow) as usize;
+        let index_con = rng.gen_range(0, len_con) as usize;
+        let c1 = vowels[index_vow];
+        let c2 = consonants[index_con];
+        res.push(c1);
+        res.push(c2);
+    }
+    return res;
+}
+
+fn generate_all(how_many: i32, size_of_str: i32) {
+    assert!(size_of_str % 2 == 0, "Size of Word must be even;\n See cargo run help");
+    // println!("\n");
+    for _ in 0..how_many {
+        println!("{}", generate(size_of_str));
+    }
+}
